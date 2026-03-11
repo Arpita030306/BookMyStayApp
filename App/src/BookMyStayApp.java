91482eae75feec745b06e3f3115ccded35f3424c
Arpita Singh <arpitasinngh050306@gmail.com>
2026-03-11 20:29:19 +0530

    Arpita Added UC1 Welcome Page

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("        WELCOME TO BOOK MY STAY        ");
+        System.out.println("=======================================");
+        System.out.println("Find and book the best rooms easily.");
+        System.out.println("Your comfortable stay starts here!");
+        System.out.println("=======================================");
+
+    }
+}
